129ddb3326fc280d5ed9de253cab9607b65e76f6
Sajay Antony <sajaya@microsoft.com>
2019-01-17 15:21:48 -0800

    Updates

---
+++ precache-manifest.e86d411022744983459af6d4e891e47d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5643c35acf4e52c91b3",
+    "url": "/image-catalog/static/css/main.3067d018.chunk.css"
+  },
+  {
+    "revision": "c5643c35acf4e52c91b3",
+    "url": "/image-catalog/static/js/main.c5643c35.chunk.js"
+  },
+  {
+    "revision": "d0bba6d1eb76f0560222",
+    "url": "/image-catalog/static/js/1.d0bba6d1.chunk.js"
+  },
+  {
+    "revision": "e41fd09c0c9f01e9ce08",
+    "url": "/image-catalog/static/js/runtime~main.e41fd09c.js"
+  },
+  {
+    "revision": "27c94a23da95f2bf8f2ef099c4ab332f",
+    "url": "/image-catalog/index.html"
+  }
+];
